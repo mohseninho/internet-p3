4d4c8b46514ecdcbf6dc7a506155fb44c161ed04
mohseninho <mohsenmsmdev@gmail.com>
2025-05-13 18:41:29 +0330

    add calculator

---
+++ calc.js
@@ -0,0 +1,17 @@
+function appendValue(val) {
+    document.getElementById('result').innerText += val;
+}
+
+function clearResult() {
+    document.getElementById('result').innerText = '';
+}
+
+function calculate() {
+    try {
+        let expression = document.getElementById('result').innerText;
+        let result = eval(expression);
+        document.getElementById('result').innerText = result;
+    } catch (error) {
+        document.getElementById('result').innerText = 'خطا';
+    }
+}
